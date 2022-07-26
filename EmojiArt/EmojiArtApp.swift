1e4628ec6e372386d9631ce4edac5809b50ce23b
valenlyn <valenlynchua@gmail.com>
2022-07-26 12:14:24 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Valenlyn Chua on 26/7/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
